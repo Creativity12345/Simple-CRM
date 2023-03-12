b7047427c2021c2d7b293e5049ab3d48a1ca826d
Creativity12345 <sebastianm444@gmail.com>
2023-03-12 10:34:48 +0100

    connect simple crm with firebase user collection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
@@ -11,8 +12,19 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 export class UserComponent {
 
   user = new User();
+  allUsers = [];
 
-  constructor(public dialog: MatDialog){}
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore){}
+
+  ngOnInit(): void {
+    this.firestore
+    .collection('users') //Sammlung aus firestore angeben
+    .valueChanges() //wenn sich etwas in der Datenbank ändert wird die Sammlung automatisch neu abonniert und aktualisiert
+    .subscribe((changes: any) => {
+      console.log(changes);
+      this.allUsers = changes;
+    });
+  }
 
   openDialog(){
     this.dialog.open(DialogAddUserComponent);
